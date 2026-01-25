fb8a6380d3fc1e174f8e41eeceda667e01e5f43b
Dev Armando Rocha <mandoqxo@gmail.com>
2026-01-25 20:29:36 -0300

    Delete IdeaProjects/EstudoAvancadoJava/src/Lambdas directory

--- IdeaProjects/EstudoAvancadoJava/src/Lambdas/Calculo.java
+++
@@ -1,6 +0,0 @@
-public interface Calculo {
-	public default double executar(double a, double b){
-		return 0;
-	}
-
-}
--- IdeaProjects/EstudoAvancadoJava/src/Lambdas/Multiplicar.java
+++
@@ -1,5 +0,0 @@
-public class Multiplicar {
-	public double executar(double a, double b) {
-		return a * b;
-	}
-}
