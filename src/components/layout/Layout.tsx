5856bb61839398e4f867edd488eb7cdc868f5788
Armandorocha13 <mandoqxo@gmail.com>
2026-01-28 13:16:29 -0300

    feat: adiciona modal de boas-vindas com instruções de compra e informações de pagamento

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
+import { WelcomeModal } from '../home/WelcomeModal';
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,6 +10,7 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col">
+      <WelcomeModal />
       <Navbar />
       <main className="flex-1">
         {children}
